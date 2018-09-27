008c7b13cf15b0028b2860d0d2b46cd757273bb4
Nico <gidudunicholas11@gmail.com>
2018-09-27 12:17:52 -0700

    Changed the HTML for the site.

---
+++ mysite/mine.py
@@ -0,0 +1,18 @@
+class person:
+	__name =''
+	__email = ''
+
+	def set_name(self: name):
+		self.__name = name
+	def get_name(self):
+			return self.name
+	def set_email(self: email):
+		self.__email = email
+	def get_name(self):
+			return self.__email			
+nico = person()
+nico.set_name('Nico Walter')
+
+print(nico)	
+
+		
